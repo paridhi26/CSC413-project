2694cd5f04503acfd978b7e852b85ab7e8285915
Li Mengyuan <limeng72@teach.cs.toronto.edu>
2024-04-19 10:43:54 -0400

    add organized models for run_adv

--- cifar10/resnet32/run_adv.sh
+++ cifar10/resnet32/run_adv.sh
@@ -3,8 +3,8 @@ dataset='mnist' # if the model is trained on CIFAR, fine-tuned with MNIST, use "
 PYTHON="python3 -m"
 
 data_path='./data'
-chk_path=./save_aug/cifar60_ft/vgg_mm_wrob.tar #path of model
-save_path=./save_adversarial
+chk_path=./save_aug/cifar60_ft/vgg_mm_wrob.tar # path of model, notice: cifar60_ft is for cifar and mnist, mnist directory is for trained only on mnist
+save_path=./save_adversarial # this is the path where it used to store the result (adv_ex.txt). NOTICE: the result got overwritten everytime run the script
 
  srun -p csc413 --gres gpu \
  $PYTHON adv_ex --dataset ${dataset} \
