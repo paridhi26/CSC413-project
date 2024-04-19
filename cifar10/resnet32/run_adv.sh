6356688ed8bba7e713fea227507b1ecb9b56ca06
Li Mengyuan <limeng72@teach.cs.toronto.edu>
2024-04-19 10:41:53 -0400

    add organized models for run_adv

--- cifar10/resnet32/run_adv.sh
+++ cifar10/resnet32/run_adv.sh
@@ -1,9 +1,9 @@
 model=vgg16_quan
-dataset='mnist' # trained on CIFAR, fine-tuned with MNIST
+dataset='mnist' # if the model is trained on CIFAR, fine-tuned with MNIST, use "finetune_mnist", "mnist" otherwise
 PYTHON="python3 -m"
 
 data_path='./data'
-chk_path=./save_aug/cifar60_ft/vgg_mm_wrob.tar
+chk_path=./save_aug/cifar60_ft/vgg_mm_wrob.tar #path of model
 save_path=./save_adversarial
 
  srun -p csc413 --gres gpu \
